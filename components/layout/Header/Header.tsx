de915d01daaa89f2709bcb88a42b6a59aee8c4e7
Joel Paucarima Franco <jopau3112@gmail.com>
2022-04-24 21:17:44 +0200

    fix(Header): change import next/link

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Link from 'next/Link';
-import Image from 'next/Image';
+import Link from 'next/link';
+import Image from 'next/image';
 import cn from 'classnames';
 import { Space } from '@/components/layout';
 import styles from '@/components/layout/Header/Header.module.scss';
