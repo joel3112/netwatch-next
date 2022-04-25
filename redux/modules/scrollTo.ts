c72464ab9f01924a586e2b52afaf35248386c035
Joel Paucarima Franco <jopau3112@gmail.com>
2022-04-25 22:17:10 +0200

    feat(scroll): add module redux + test

---
+++ redux/modules/scrollTo.ts
@@ -0,0 +1,62 @@
+import { Action } from 'redux';
+
+export type ScrollToState = {
+  container: HTMLDivElement | null;
+  scrollPosition: number;
+  forceTop: boolean;
+};
+
+type ScrollToAction = Action & {
+  payload: Partial<ScrollToState>;
+};
+
+// Constants
+export const initialValue: ScrollToState = {
+  container: null,
+  scrollPosition: 0,
+  forceTop: false
+};
+
+// Actions
+export enum actionTypes {
+  RESET = 'scrollTo/RESET',
+  SCROLL = 'scrollTo/SCROLL'
+}
+
+// Reducer
+export default function reducer(state = initialValue, action: ScrollToAction): ScrollToState {
+  const { payload } = action;
+
+  switch (action.type) {
+    case actionTypes.RESET:
+      return { ...state, ...payload, forceTop: true };
+    case actionTypes.SCROLL:
+      return {
+        ...state,
+        ...payload,
+        scrollPosition: Number(payload.scrollPosition),
+        forceTop: false
+      };
+    default:
+      return state;
+  }
+}
+
+// Action creators
+export const actions = {
+  reset(container: HTMLDivElement): ScrollToAction {
+    return { type: actionTypes.RESET, payload: { container } };
+  },
+  scroll(container: HTMLDivElement, position: number): ScrollToAction {
+    return { type: actionTypes.SCROLL, payload: { container, scrollPosition: position } };
+  }
+};
+
+// Subscribe changes
+export const onChangeScrollToState = ({ scrollTo }: { scrollTo: ScrollToState }) => {
+  const { container, forceTop } = scrollTo;
+
+  if (forceTop && container) {
+    container.scrollTo && container.scrollTo(0, 0);
+  }
+};
