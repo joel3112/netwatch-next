649b739249af1e6024e246ff00a9070094d3e9ed
Joel Paucarima Franco <jopau311289@gmail.com>
2022-04-28 16:50:25 +0200

    feat(button): add component + test

---
+++ components/forms/Button/Button.tsx
@@ -0,0 +1,65 @@
+import { MouseEvent, MouseEventHandler } from 'react';
+import cn from 'classnames';
+import { ElementChildren, ElementHTML, ElementSkeleton } from '@/types';
+import { withSkeleton } from '@/hoc/withSkeleton';
+import { withTooltip } from '@/hoc/withTooltip';
+import styles from '@/components/forms/Button/Button.module.scss';
+
+export type ButtonProps = Partial<typeof defaultProps> &
+  ElementHTML &
+  ElementChildren &
+  ElementSkeleton & {
+    size?: 'small' | 'medium' | 'large' | 'xlarge';
+    ariaLabel?: string;
+    tooltip?: string;
+    secondary?: boolean;
+    clear?: boolean;
+    outline?: boolean;
+    disabled?: boolean;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+  };
+
+const defaultProps = {
+  size: 'medium'
+};
+
+const Button = ({
+  className,
+  children,
+  ariaLabel,
+  size,
+  secondary,
+  clear,
+  outline,
+  disabled,
+  tooltip,
+  onClick
+}: ButtonProps) => {
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    onClick && onClick(event);
+  };
+
+  return (
+    <button
+      aria-label={ariaLabel || tooltip}
+      className={cn(
+        styles.buttonWrapper,
+        size && styles[size],
+        secondary && styles.secondary,
+        clear && styles.clear,
+        outline && styles.outline,
+        className
+      )}
+      disabled={disabled}
+      onClick={handleClick}>
+      {children}
+    </button>
+  );
+};
+
+Button.defaultProps = defaultProps;
+
+const ButtonWithTooltip = withTooltip<ButtonProps>(Button);
+const ButtonWithSkeleton = withSkeleton<ButtonProps>(ButtonWithTooltip, { variant: 'button' });
+
+export default ButtonWithSkeleton;
