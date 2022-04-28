4447878a8a73759e956d5424f35457fa59ff3db4
Joel Paucarima Franco <jopau311289@gmail.com>
2022-04-28 08:04:20 +0200

    feat(withChildrenFiltered): add hoc

---
+++ hoc/withChildrenFiltered.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/display-name */
+import { ComponentType, useEffect, useState } from 'react';
+import { FunctionGeneric, ObjectGeneric } from '@/types';
+
+export interface WithChildrenFilteredProps {
+  children?: JSX.Element | Array<JSX.Element>;
+}
+
+const decorateHOCWithStaticProps = (hoc: FunctionGeneric) => {
+  return function execHOC<T extends WithChildrenFilteredProps>(
+    Component: ComponentType<T>,
+    compounds: ObjectGeneric
+  ) {
+    const c = hoc(Component, compounds);
+
+    Object.entries(compounds).forEach(([name, component]) => {
+      c[name] = component;
+    });
+
+    return c;
+  };
+};
+
+export const withChildrenFiltered = decorateHOCWithStaticProps(
+  <T extends WithChildrenFilteredProps>(
+      Component: (x: T) => JSX.Element,
+      compounds: { [name: string]: ComponentType<T> }
+    ) =>
+    ({ children, ...hocProps }: T) => {
+      const [filteredChildren, setFilteredChildren] = useState<Array<JSX.Element>>([]);
+
+      const isComponentFiltered = (child: JSX.Element) => {
+        return (
+          child.type &&
+          Object.keys(compounds).find((key) => child.type.name === compounds[key].name)
+        );
+      };
+
+      useEffect(() => {
+        if (children) {
+          if (Array.isArray(children)) {
+            setFilteredChildren(children.filter(isComponentFiltered));
+          } else {
+            setFilteredChildren(isComponentFiltered(children) ? [children] : []);
+          }
+
+          [children].flat().forEach((child: JSX.Element) => {
+            if (!isComponentFiltered(child)) {
+              const compoundsExpected = Object.keys(compounds)
+                .map((key) => (compounds[key] as ComponentType).name)
+                .join(', ');
+              throw new Error(
+                `${Component.name} children must be ${compoundsExpected}, received ${child.type}`
+              );
+            }
+          });
+        }
+      }, [children]);
+
+      return <Component {...(hocProps as T)}>{filteredChildren}</Component>;
+    }
+);
