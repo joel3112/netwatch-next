471cb73fe6e5555aab9e872347d64d6b4816f03a
Joel Paucarima Franco <jopau311289@gmail.com>
2022-04-29 00:59:08 +0200

    feat(portal): add component + test

---
+++ components/overlay/Portal/Portal.tsx
@@ -0,0 +1,106 @@
+import { cloneElement, createContext, useContext } from 'react';
+import { BackdropProps as BackdropPropsMUI, PortalProps as PortalPropsMUI } from '@mui/material';
+import cn from 'classnames';
+import { ElementChildren, ElementHTML, FunctionVoid } from '@/types';
+import { withChildrenFiltered } from '@/hoc/withChildrenFiltered';
+import { useModal } from '@/hooks/useModal';
+import styles from '@/components/overlay/Portal/Portal.module.scss';
+
+/* -------------------------------------------------------------------------- */
+/** Context **/
+/* -------------------------------------------------------------------------- */
+
+type PortalContextProps = typeof defaultValue & {
+  opened?: boolean;
+  onChange?: FunctionVoid<boolean>;
+};
+
+const defaultValue = {
+  opened: false
+};
+
+const PortalContext = createContext<PortalContextProps>(defaultValue);
+
+const usePortalContext = () => useContext(PortalContext);
+
+/* -------------------------------------------------------------------------- */
+/** PortalHandler (child component) **/
+/* -------------------------------------------------------------------------- */
+
+export type PortalHandlerProps = ElementHTML & Required<ElementChildren<JSX.Element>>;
+
+const PortalHandler = ({ children: Child }: PortalHandlerProps) => {
+  const { onChange } = usePortalContext();
+
+  return cloneElement(Child, {
+    ...Child.props,
+    onClick: () => onChange && onChange(true)
+  });
+};
+
+/* -------------------------------------------------------------------------- */
+/** PortalPortal (child component) **/
+/* -------------------------------------------------------------------------- */
+
+export type PaperModalProps = {
+  container: PortalPropsMUI['container'];
+  BackdropProps?: Partial<BackdropPropsMUI>;
+};
+
+type PortalPaperProps = ElementHTML & Required<ElementChildren<JSX.Element>>;
+
+const PortalPaper = ({ children: Child }: PortalPaperProps) => {
+  const { opened, onChange } = usePortalContext();
+  const modalConfig = {
+    container: document.querySelector('.app'),
+    BackdropProps: {
+      classes: {
+        root: styles.backdrop
+      }
+    }
+  };
+
+  return cloneElement(Child, {
+    className: cn(styles.portalPaper, Child.props && Child.props.className),
+    opened,
+    onChange: () => onChange && onChange(false),
+    modalConfig,
+    ...Child.props
+  });
+};
+
+/* -------------------------------------------------------------------------- */
+/** Portal (main component) **/
+/* -------------------------------------------------------------------------- */
+
+export type PortalProps = typeof defaultProps &
+  ElementHTML &
+  ElementChildren<Array<JSX.Element>> & {
+    opened?: boolean;
+    onChange?: FunctionVoid<boolean>;
+  };
+
+const defaultProps = defaultValue;
+
+const Portal = ({ children, opened, onChange }: PortalProps) => {
+  const { isOpened, handleChange } = useModal(opened, onChange);
+
+  return (
+    <PortalContext.Provider
+      value={{
+        opened: isOpened,
+        onChange: handleChange
+      }}>
+      <div className={cn(styles.portalWrapper, 'wrapper')}>{children}</div>
+    </PortalContext.Provider>
+  );
+};
+
+Portal.defaultProps = defaultProps;
+
+const PortalWithChildrenFiltered = withChildrenFiltered(Portal, {
+  Handler: PortalHandler,
+  Paper: PortalPaper
+});
+
+export default PortalWithChildrenFiltered;
