05a76a4c293ec86d01884d2b669f99bd07f652c6
Joel Paucarima Franco <jopau3112@gmail.com>
2022-05-16 09:28:42 +0200

    feat(*): add section page

---
+++ containers/MediaResume/MediaResume.tsx
@@ -0,0 +1,62 @@
+import { useTranslation } from 'next-i18next';
+import { FiArrowLeft } from 'react-icons/fi';
+import Link from 'next/link';
+import { ElementHTML, MediaImageRatio, MovieDetail, TVDetail } from '@/types';
+import { Heading, Text } from '@/components/typography';
+import { Image } from '@/components/media';
+import { Container, Space } from '@/components/layout';
+import { Button } from '@/components/forms';
+import { Card } from '@/components/display';
+import { classes } from '@/utils/helpers';
+import styles from '@/containers/MediaResume/MediaResume.module.scss';
+
+export type MediaResumeProps = typeof defaultProps &
+  ElementHTML & {
+    media?: MovieDetail | TVDetail;
+  };
+
+const defaultProps = {
+  media: {}
+};
+
+const MediaResume = ({ media }: MediaResumeProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classes(styles.wrapper, 'full', 'dark')}>
+      <Image alt="backdrop" src={media.backdrop || ''} width="100%" />
+
+      <Space justify="center" className={styles.body} direction="column" gap={2}>
+        <Container>
+          <Space gap={25} direction="row">
+            <Card>
+              <Image alt="image" src={media.image} width={110} ratio={MediaImageRatio.POSTER} />
+            </Card>
+
+            <Space gap={10} direction="column">
+              <Heading level={3}>{media.name}</Heading>
+              <Text size="sm">{`${media.date} - ${media.duration}`}</Text>
+
+              <Link
+                href={{
+                  pathname: '/[type]/[id]',
+                  query: { type: media.type, id: media.id }
+                }}>
+                <a>
+                  <Button className={styles.buttonBack} clear>
+                    <FiArrowLeft />
+                    {t('main.go.back.button')}
+                  </Button>
+                </a>
+              </Link>
+            </Space>
+          </Space>
+        </Container>
+      </Space>
+    </div>
+  );
+};
+
+MediaResume.defaultProps = defaultProps;
+
+export default MediaResume;
---
+++ containers/MediaSection/index.ts
@@ -0,0 +1,1 @@
+export { default as MediaSection } from '@/containers/MediaSection/MediaSection';
